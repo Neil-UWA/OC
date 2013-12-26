65b3cacbbd39da8d6cb3169a79221bad733013f3
Neil <neil-lu@graduate.uwa.edu.au>
2013-12-26 13:11:30 +0800

    added the default route to the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 LearningApp::Application.routes.draw do
+  root "bootstrap#home"
   get "bootstrap/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
