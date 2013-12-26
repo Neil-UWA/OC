177e4d00db181dc01a879b52de69937b1823447a
Neil <neil-lu@graduate.uwa.edu.au>
2013-12-26 13:09:23 +0800

    added a home page

---
+++ app/controllers/bootstrap_controller.rb
@@ -0,0 +1,4 @@
+class BootstrapController < ApplicationController
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 LearningApp::Application.routes.draw do
+  get "bootstrap/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
