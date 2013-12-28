b4fd9df4ea864cfa9d6335a79fbe5a64f85e62ab
Neil <neil-lu@graduate.uwa.edu.au>
2013-12-28 14:10:23 +0800

    added a sessions controller, added a signin page, need to implement the session storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 LearningApp::Application.routes.draw do
 
 	resources :users
+	resources :sessions, only:[:create, :new, :destroy]
 
   root "home#index"
+
 	get 'home', to:'home#index'
+	get 'signup', to: 'users#new'
+	get 'signin', to: 'sessions#new'
 
 end
