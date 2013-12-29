a95eae8ab7fa21f7186bde433c766494a8a0a145
Neil <neil-lu@graduate.uwa.edu.au>
2013-12-29 20:37:49 +0800

    made the email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
 	has_secure_password
 
 	#emial validation regular expression
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :name, 
-						:email, 
+						:email,  
 						:password_confirmation,
 						:password, presence: true 
 
-	validates :email, format: {with:email_regex}
+	validates :email, format: { with:email_regex }, uniqueness: { case_sensitive:false }
 
-	validates :password, length: {minimum:6},
-												confirmation: true
+	validates :password, length: { minimum:6 }, confirmation: true
 
 end
