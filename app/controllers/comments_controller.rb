3e6590335f56d361ef79c08741a8a1d082dfece1
Neil <neil-lu@graduate.uwa.edu.au>
2014-01-03 10:12:24 +0800

    show comment and post created time on the post show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
 		@post = Post.find(params[:post_id])
-		@comment = @post.comments.build(params[:comment][:comment])
+		@comment = @post.comments.build(params[:comment].permit(:comment))
 		@comment.user = current_user
 
 		# TODO 
