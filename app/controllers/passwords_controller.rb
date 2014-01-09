ae6451edf0554c6d8fa04624efc9b3e2de1e5bba
Neil <neil-lu@graduate.uwa.edu.au>
2014-01-09 22:31:53 +0800

    haha

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -22,7 +22,7 @@ class PasswordsController < ApplicationController
 		
 		if @user.password_reset_sent_at < 0.5.hours.ago
 			redirect_to root_path, notice: "Password reset session expired"
-		elsif @user.update_attribute(password:params[:user][:password])
+		elsif @user.update_attribute(:password, params[:user][:password])
 			redirect_to root_path, success: "Password has been reset"
 		else
 			render :edit
